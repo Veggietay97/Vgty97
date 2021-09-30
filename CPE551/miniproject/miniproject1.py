8e9a19eed3001637280c93dbfb697a944750a584
Veggietay97 <60272337+Veggietay97@users.noreply.github.com>
2021-09-30 13:34:19 -0400

    Add files via upload

---
+++ CPE551/miniproject/miniproject1.py
@@ -0,0 +1,21 @@
+print("Choose a noun:")
+
+noun = input()
+
+print("Choose a plural noun:")
+
+plural_noun = input()
+
+print("Name a place:")
+
+place = input()
+
+print("Choose an adjective:")
+
+adjective = input()
+
+print("Choose a noun:")
+
+noun2 = input()
+
+print("Hey,",noun,",your", adjective, plural_noun, "just turned into",noun2,"at", place)
